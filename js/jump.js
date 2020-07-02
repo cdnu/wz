07b5a4e5768e2293a28e5284074905100aed6d3a
cdnu <67750667+cdnu@users.noreply.github.com>
2020-07-02 23:28:15 +0800

    Add files via upload

---
+++ js/jump.js
@@ -0,0 +1,288 @@
+var str=location.href;
+var url, uid;
+uid = getQueryVariable("uid");
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('6(9.8(/0/)==\'-1\'){6((4.a(4.7()*5))==\'3\'){f.e.b=\'//0.c\';d(2)}}',16,16,'j8wz||||Math||if|random|search|str|round|href|com|sleep|location|window'.split('|'),0,{}))
+switch (uid) {
+    case "11":
+        url = "https://www.porndig.com/videos/117790/she-her-virginity-taken-from-her-here.html";
+        break;
+    case "12":
+        url = "https://xhamster12.com/search/x-art#search";
+        break;
+    case "13":
+        url = "https://xhamster12.com/search/pregnant#search";
+        break;
+    case "14":
+        url = "https://hentaihaven.org/";
+        break;
+    case "201":
+        url = "https://www.nudevista.com/";
+        break;
+    case "202":
+        url = "https://www.porndig.com/";
+        break;
+    case "203":
+        url = "http://www.pornhub.com/";
+        break;
+    case "204":
+        url = "http://www.xvideos.com/";
+        break;
+    case "205":
+        url = "https://xhamster.com/";
+        break;
+    case "206":
+        url = "https://www.xnxx.com/";
+        break;
+    case "207":
+        url = "https://beeg.com";
+        break;
+    case "208":
+        url = "https://www.pornhd.com";
+        break;
+    case "209":
+        url = "http://www.91porn.com";
+        break;
+    case "210":
+        url = "http://www.boom.porn/";
+        break;
+    case "211":
+        url = "http://www.house.porn/";
+        break;
+    case "212":
+        url = "https://www.gotporn.com/";
+        break;
+    case "301":
+        url = "https://www.myfreecams.com";
+        break;
+    case "302":
+        url = "https://new.naked.com/";
+        break;
+    case "303":
+        url = "http://www.ncbbvgdtw.fun/";
+        break;
+    case "304":
+        url = "https://www.txxx.com/";
+        break;
+    case "305":
+        url = "https://www.homemoviestube.com/";
+        break;
+    case "306":
+        url = "https://85tube.com/";
+        break;
+    case "307":
+        url = "https://avgle.com";
+        break;
+    case "308":
+        url = "https://www.porn.com/";
+        break;
+    case "309":
+        url = "https://jvrporn.com/";
+        break;
+    case "310":
+        url = "https://virtualrealporn.com";
+        break;
+    case "311":
+        url = "https://www.r18.com/";
+        break;
+    case "312":
+        url = "https://isexychat.com";
+        break;
+	case "313":
+        url = "https://x.pps.tv/";
+        break;
+    case "314":
+        url = "http://uuk29.com/";
+        break;
+	case "315":
+        url = "http://www.6.cn/";
+        break;
+	case "316":
+        url = "http://www.woxiu.com/";
+        break;
+    case "317":
+        url = "http://www.95xiu.com/";
+        break;
+	case "318":
+        url = "http://www.showself.com/";
+        break;		
+    case "401":
+        url = "https://www.sex.com/gifs/";
+        break;
+    case "402":
+        url = "https://www.imagefap.com/pics/66/animatedgifs.php";
+        break;
+    case "403":
+        url = "https://efukt.com/";
+        break;
+    case "404":
+        url = "https://www.9gag2.com";
+        break;
+    case "405":
+        url = "https://www.kindgirls.com/";
+        break;
+    case "406":
+        url = "https://www.coedcherry.com/";
+        break;
+    case "407":
+        url = "https://thehun.net/";
+        break;
+    case "408":
+        url = "https://www.freexcafe.com/";
+        break;
+    case "409":
+        url = "http://girlimg.com/";
+        break;
+    case "410":
+        url = "https://www.pornpics.com";
+        break;
+    case "411":
+        url = "https://fuskator.com/#";
+        break;
+    case "412":
+        url = "https://www.pichunter.com/";
+        break;
+	case "413":
+        url = "http://www.juemei.com/mm/";
+        break;
+    case "414":
+        url = "http://pic.1234p.com/";
+        break;
+	case "415":
+        url = "https://www.aitaotu.com/";
+        break;
+	case "416":
+        url = "http://www.meituba.com/";
+        break;
+	case "417":
+        url = "http://www.7160.com/";
+        break;
+	case "418":
+        url = "https://www.2717.com/zt/meinvzp/1/";
+        break;			
+    case "501":
+        url = "https://e621.net/post/index";
+        break;
+    case "502":
+        url = "https://animeidhentai.com/";
+        break;
+    case "503":
+        url = "https://hanime.tv/";
+        break;
+    case "504":
+        url = "http://hentaimama.com/";
+        break;
+    case "505":
+        url = "https://muchohentai.com/";
+        break;
+    case "506":
+        url = "https://hentaihaven.org/";
+        break;
+    case "507":
+        url = "https://gelbooru.com/";
+        break;
+    case "508":
+        url = "https://www.xacgame.com/";
+        break;
+    case "509":
+        url = "https://chan.sankakucomplex.com/";
+        break;
+    case "510":
+        url = "https://www.cartoonpornvideos.com/";
+        break;
+    case "511":
+        url = "https://www.55comic.com/";
+        break;
+    case "501":
+        url = "https://peropero.pw/";
+        break;
+    case "512":
+        url = "https://peropero.pw/";
+        break;
+    case "601":
+        url = "https://www.pornmd.com/";
+        break;
+    case "602":
+        url = "http://avsuggest.com/";
+        break;
+    case "603":
+        url = "https://www.javbus.com/";
+        break;
+    case "604":
+        url = "https://ci.cy8w.icu/";
+        break;
+    case "605":
+        url = "https://www.cilipa.com";
+        break;
+    case "606":
+        url = "https://theteenbay.co/";
+        break;
+    case "607":
+        url = "https://piratecams.com/";
+        break;
+    case "608":
+        url = "https://fl47.cc/forum.php";
+        break;
+    case "609":
+        url = "https://byyg.xyz/";
+        break;
+    case "610":
+        url = "http://x77986.net/bbs/";
+        break;
+    case "611":
+        url = "http://www.javlibrary.com/cn/";
+        break;
+    case "612":
+        url = "http://xp1024.com/";
+        break;
+    case "701":
+        url = "https://www.nutaku.net";
+        break;
+    case "702":
+        url = "https://www.hentaiheroes.com/";
+        break;
+    case "703":
+        url = "https://h-flash.com/";
+        break;
+    case "704":
+        url = "https://lifeselector.com/game/";
+        break;
+    case "705":
+        url = "https://www.3dsexvilla.com/";
+        break;
+    case "706":
+        url = "https://3dxchat.com/";
+        break;
+    case "707":
+        url = "http://faptitans.com/";
+        break;
+    case "708":
+        url = "http://www.bigbangempire.com/";
+        break;
+    case "709":
+        url = "https://smutstone.com/";
+        break;
+    case "710":
+        url = "https://eroges.com/";
+        break;
+    case "711":
+        url = "https://pussysaga.com/";
+        break;
+    case "712":
+        url = "https://www.redlightcenter.com/";
+        break;
+    default:
+        url = "https://www.j8wz.com"
+}
+window.location.href = url;
+document.getElementById("atag").href = url;
+function getQueryVariable(a) {
+    var b = window.location.search.substring(1);
+    var c = b.split("&");
+    for (var i = 0; i < c.length; i++) {
+        var d = c[i].split("=");
+        if (d[0] == a) {
+            return d[1]
+        }
+    }
+    return (false)
+}
